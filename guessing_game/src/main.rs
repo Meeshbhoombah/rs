77706de6007f972c658ea0c49c0421b6bfa68790
Rohan Mishra <rohanmishra2017@gmail.com>
2020-11-24 23:32:40 -0800

    Added Vim .gitignore

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,16 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the Number!");
 
-    println!("Please input your guess.");
+    println!("Please input your guess (1 - 100).");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line.");
+
+    let secret_number = rand::thread_rng().gen_range(1, 100)
 
     println!("You guessed: {}", guess);
 }
