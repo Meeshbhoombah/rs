f0a3a734f5a87a546b70a81bc83830c322ed3853
Rohan Mishra <rohanmishra2017@gmail.com>
2020-11-25 17:14:37 -0800

    Added infinite guesses until user wins

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,33 @@
 use std::io;
+
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the Number!");
 
-    println!("Please input your guess (1 - 100).");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let secret_number = rand::thread_rng().gen_range(1, 100)
+    loop {
+        println!("Please input your guess (1 - 100).");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    println!("You guessed: {}", guess);
+        // we recieve input as a String, conversion enables comparison w/ integers 
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
