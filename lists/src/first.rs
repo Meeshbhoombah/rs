0507c522ea4bf87a4b9913b8d4859b47fcf1791d
Rohan Mishra <rohanmishra2017@gmail.com>
2021-01-19 23:25:18 -0800

    Fixed struct, starting push method

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,5 +1,26 @@
-pub enum List {
+struct Node {
+    elem: i32,
+    next: List,
+}
+
+enum Link {
     Empty,
-    Elem(i32, Box<List>),
+    More(Box<Node>),
+}
+
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { 
+            head: Link::Empty
+        }
+    }
+
+    pub fn push(&mut self. elem: i32) {
+        // TODO
+    }
 }
 
