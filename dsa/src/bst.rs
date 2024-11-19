c0c47434d18dbab6c84c5786a7d942c11c0b9c19
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-19 02:38:48 -0500

    Added two insert functions

--- dsa/src/bst.rs
+++ dsa/src/bst.rs
@@ -12,6 +12,56 @@ impl<T> BinarySearchTree<T>
 where
     T: Ord,
 {
+    pub fn insert(&mut self, value: T) {
+        match &self.value {
+            None => self.value = Some(value),
+            Some(key) => {
+                let target_node = if value < *key {
+                    &mut self.left
+                } else {
+                    &mut self.right
+                };
+                match target_node {
+                    Some(ref mut node) => {
+                        node.insert(value);
+                    }
+                    None => {
+                        let mut node = new();
+                        node.value = Some(value);
+                        *target_node = Some(Box::new(node));
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+    pub fn insert2(&mut self, x: T) {
+        match &self.value {
+            None => {
+                self.value = Some(x);
+                self.count += 1;
+            }
+            Some(comparable) => {
+                if x < *comparable {
+                    let left = &mut self.left;
+                    match left {
+                        Some(ref mut node) => {
+                            node.insert(x);
+                        }
+                        None => {
+                            let mut node = new();
+                            node.value = Some(x);
+                            *left = Some(Box::new(node));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    */
+
+    /*
     pub fn insert(&mut self, x: T) {
         if self.value.is_none() {
             self.value = Some(x); 
@@ -22,9 +72,10 @@ where
         if x < comparable {
             if self.left.is_none() {
                 self.left = Some(Box::new(new()));
-            } 
-            
-            self.left.insert(Box::new(new()));
+            }
+
+            let left = &mut self.left;
+            left.insert(x);
         } 
 
         if x == comparable {
@@ -35,6 +86,7 @@ where
             self.right.insert(x);
         }
     }
+    */
 }
 
 pub fn new<T>() -> BinarySearchTree<T>
