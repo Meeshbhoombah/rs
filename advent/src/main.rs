f46937b777b2eca4a47ed269b10a33117b100353
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-12-01 12:47:22 -0500

    Working on test list but not input

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -11,16 +11,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("The current directory is {}", path.display());
 
     // Raw file input
-    let input: String = fs::read_to_string("day_one_test.txt")?;
-    // let input: String = fs::read_to_string("day_one_input.txt")?;
+    // let input: String = fs::read_to_string("day_one_test.txt")?;
+    let input: String = fs::read_to_string("day_one_input.txt")?;
     // println!("{}", input);
 
 
-    let mut current_list = 0;
+    let mut lists_length = 0;
     let mut list_zero = BinaryHeap::new();
     let mut list_one = BinaryHeap::new();
 
 
+    let mut current_list = 0;
     let pairs = input.split("\n");
     for pair in pairs {
         let elements = pair.split("   ");
@@ -38,15 +39,48 @@ fn main() -> Result<(), Box<dyn Error>> {
                 list_one.push(Reverse(element));
                 current_list = 0;
             }
+
+
         }
+            
+        lists_length += 1;
     }
+
 
     // For some reason, inputting all values into both min_heaps adds a "" 
     // value to `list_zero` that we do not need
     list_zero.pop();
-
+    lists_length -= 1;
+   
+    /*
+    println!("{:?}", lists_length);
     println!("{:?}", list_zero);
     println!("{:?}", list_one);
+    */
+
+
+    let mut summed_location_difference = 0;
+
+    let mut i = 0;
+    while i < lists_length {
+        println!("zero: {:?}", list_zero.peek());
+        println!("one: {:?}", list_one.peek());
+
+
+        // `Reversed` is a tuple
+        let location_difference = list_one.pop().unwrap().0.parse::<i128>().unwrap() - list_zero.pop().unwrap().0.parse::<i128>().unwrap();
+        println!("location_difference: {}", location_difference);
+        summed_location_difference += location_difference;
+
+        i += 1;
+
+        println!("--- NEXT PAIR ---");
+    }
+
+
+    println!("RESULT:");
+    println!("{}", summed_location_difference);
+
 
     Ok(())
 
