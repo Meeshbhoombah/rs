4b98149c7274ff42b291ef3d9da9e0e57684ebff
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-12-01 11:22:36 -0500

    Added reqwest

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+const INPUT: &str = "https://adventofcode.com/2024/day/1/input";
+
+async fn main() -> Result<> {
+    let body = reqwest::get(INPUT)
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {body:?}");
 }
+
