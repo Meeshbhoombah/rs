b769c98c627e7e63a0642eac5b686c5f29518d0e
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-12-01 11:27:57 -0500

    Added all tokio features

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
+
 const INPUT: &str = "https://adventofcode.com/2024/day/1/input";
 
-async fn main() -> Result<> {
+#[tokio::main]
+async fn main() -> Result<(), reqwet::Error> {
     let body = reqwest::get(INPUT)
         .await?
         .text()
