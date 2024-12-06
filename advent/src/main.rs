04fabe2e0828b1eafd07d65c7a4d3e2ed92b01e5
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-12-06 11:27:39 -0500

    Started day six

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -2,8 +2,8 @@ use std::fs;
 
 fn main() {
 
-    // let input = fs::read_to_string("./day_five_input.txt").unwrap();
-    let input = fs::read_to_string("./day_five_test.txt").unwrap();
+    // let input = fs::read_to_string("./day_sixe_input.txt").unwrap();
+    let input = fs::read_to_string("./day_six_test.txt").unwrap();
     println!("{:?}", input);
 
 }
